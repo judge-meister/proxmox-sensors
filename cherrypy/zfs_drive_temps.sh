39a56115793e3726af3e63af72676476c7e68519
Ian <ianjudge1969@gmail.com>
2021-05-01 23:31:31 +0100

    added abs path to zpool and smartctl

--- cherrypy/zfs_drive_temps.sh
+++ cherrypy/zfs_drive_temps.sh
@@ -2,16 +2,16 @@
 
 export ZPOOL_SCRIPTS_AS_ROOT=1
 
-zpool list | grep -v 'NAME' | awk -F' ' '{print $1}' | while read zp;
+/usr/sbin/zpool list | grep -v 'NAME' | awk -F' ' '{print $1}' | while read zp;
 do
   if [ "$zp" != "no" ]
   then
 
   echo -e "\n"$zp":"
-  zpool status $zp | egrep 'ata-' | awk -F' ' '{print $1}' | while read x; 
+  /usr/sbin/zpool status $zp | egrep 'ata-' | awk -F' ' '{print $1}' | while read x; 
   do 
     d=$(ls -l /dev/disk/by-id/$x | awk -F' ' '{print $11}' | cut -d'/' -f3)
-    t=$(smartctl -l scttempsts /dev/$d | grep Current | awk -F' ' '{print $3}')
+    t=$(/usr/sbin/smartctl -l scttempsts /dev/$d | grep Current | awk -F' ' '{print $3}')
   
     echo $d $t Celsius $x
   done
@@ -20,17 +20,17 @@ do
 done
 
 echo -e "\nOthers:"
-zd=$(zpool list | grep -v NAME | awk -F' ' '{print $1}' | while read zp; do zpool status $zp | grep 'ata-'| awk -F' ' '{print $1}' | while read x; do d=$(ls -l /dev/disk/by-id/$x | awk -F' ' '{print $11}' | cut -d'/' -f3); echo " -e "$d; done; done)
+zd=$(/usr/sbin/zpool list | grep -v NAME | awk -F' ' '{print $1}' | while read zp; do /usr/sbin/zpool status $zp | grep 'ata-'| awk -F' ' '{print $1}' | while read x; do d=$(ls -l /dev/disk/by-id/$x | awk -F' ' '{print $11}' | cut -d'/' -f3); echo " -e "$d; done; done)
 
 for d in $(ls /dev/sd? | cut -d'/' -f3 | egrep -v ${zd});
 do
     #d=$(ls -l /dev/disk/by-id/$x | awk -F' ' '{print $11}' | cut -d'/' -f3)
-    t=$(smartctl -l scttempsts /dev/$d | grep Current | awk -F' ' '{print $3}')
+    t=$(/usr/sbin/smartctl -l scttempsts /dev/$d | grep Current | awk -F' ' '{print $3}')
     id=$(ls -l /dev/disk/by-id/ | grep 'ata-.*'${d}'$' | awk -F' ' '{print $9}')  
     echo $d $t Celsius $id
   
 done
 
 id=$(ls -l /dev/disk/by-id/ | grep 'nvme-.*' | grep -v -e 'eui\.' -e 'part' | awk -F' ' '{print $9}')
-echo "nvme0 "$(smartctl -a /dev/nvme0 | grep Temperature: | awk -F' ' '{print $2}')" Celsius "$id
+echo "nvme0 "$(/usr/sbin/smartctl -a /dev/nvme0 | grep Temperature: | awk -F' ' '{print $2}')" Celsius "$id
 
