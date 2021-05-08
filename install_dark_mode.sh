5b0666cf3e8d70b24d91a9620746643440e4975d
Ian <ianjudge1969@gmail.com>
2021-05-08 13:00:11 +0100

    added sudo to install_dark_mode.sh

--- install_dark_mode.sh
+++ install_dark_mode.sh
@@ -3,5 +3,5 @@
 rm -f PVEDiscordDark.sh
 
 wget https://raw.githubusercontent.com/Weilbyte/PVEDiscordDark/master/PVEDiscordDark.sh
-bash PVEDiscordDark.sh install
+sudo bash PVEDiscordDark.sh install
 
